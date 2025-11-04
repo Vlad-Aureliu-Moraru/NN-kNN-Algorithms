60ce298fa7260445b103eb0b9666978258c7c92d
thorn <moraru.vlad34@gmail.com>
2025-11-04 23:55:59 +0200

    face recognition 1.2

--- algorithm_related.py
+++ algorithm_related.py
@@ -11,8 +11,8 @@ data_path = 'orl/'
 h, w = 112, 92
 num_pixels = h * w          
 num_persons = 40
-num_train_per_person = 8
-num_total_train = num_persons * num_train_per_person  
+#num_train_per_person = 10
+#num_total_train = num_persons * num_train_per_person  
 
 
 def displayImages(poza_test,knn_result,nn_result):
@@ -41,6 +41,9 @@ def constructTrainingMatrix(num_persons,num_img_per_person):
     coloane_B = []
     labels_A = []  
     labels_B = []
+    count = 0
+    countB = 0
+    num_total_train = num_persons * num_img_per_person  
     A = np.zeros((num_pixels, num_total_train), dtype=np.float32)
     B = np.zeros((num_pixels,num_total_train) , dtype = np.float32)
     for i in range(1, num_persons + 1):          
@@ -51,13 +54,16 @@ def constructTrainingMatrix(num_persons,num_img_per_person):
             img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
             img = img.astype(np.float32)
             img_vector = img.reshape(num_pixels, 1)
-            chance  = random.randint(1,3) 
-            if chance<=2:
+            if j<=8:
                 coloane_A.append( img_vector[:, 0])
                 labels_A.append(i)
+                count+=1
             else:
                 coloane_B.append( img_vector[:, 0])
                 labels_B.append(i)
+                countB+=1
+    print(count)
+    print(countB)
 
     A = np.column_stack(coloane_A)
     B = np.column_stack(coloane_B)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import algorithm_related as alg
 import fileResultsWriter as frw
+import matplotlib.pyplot as plt
 import time 
 h, w = 112, 92
 
 
-A,B,labels_A,labels_B = alg.constructTrainingMatrix(40,8)
+A,B,labels_A,labels_B = alg.constructTrainingMatrix(40,10)
 
 nn_count_correct = 0
 knn_count_correct = 0
@@ -12,20 +13,22 @@ knn_count_correct = 0
 nn_result_array = []
 knn_result_array = []
 
-person_num = (B.size/10304)
+person_num = B.shape[1]
 start = time.time()
 for i in range (int(person_num)):
     vectorized_img = B[:,i]
     poza_test = vectorized_img.reshape(h, w)
     label,io = alg.kNN(vectorized_img, A,labels_A, 2, 3)  
     knn_result = A[:, io].reshape(h, w)
-    if labels_A[io] == labels_B[i]:
+    
+    if label == labels_B[i]:
         knn_result_array.append(True)
         knn_count_correct+=1
     else:
         knn_result_array.append(False)
     i1 =alg.NN(vectorized_img, A, 2)       
     nn_result = A[:, i1].reshape(h, w)
+
     if labels_A[i1] == labels_B[i]:
         nn_result_array.append(True)
         nn_count_correct+=1
@@ -36,14 +39,16 @@ end = time.time()
 frw.append_results(nn_result_array,knn_result_array)
 total_avg_nn,total_avg_knn = frw.read_and_average()
 timpul_de_executie = end-start
+nn_rate = nn_count_correct / B.shape[1]
+knn_rate = knn_count_correct / B.shape[1]
+
 print("---=====STATISTICA=====---")
-print(f"• Rata Totala De Recunoasteri (%) KNN {total_avg_knn}%")
-print(f"• Rata Totala De Recunoasteri (%)  NN {total_avg_nn}%")
 print(f"• Timpul de executare al algoritmului pentru {person_num:.0f} persoane {timpul_de_executie:.4f}s")
-print(f"• Timpul Aproximat De Executie Pentru Procesarea Unei Imagini {timpul_de_executie/person_num}s")
-print(f"• Matricea De Antrenare Contine : {320-person_num:.0f} imagini ")
-print(f"• Rata de Recunoastere Pentru Acest Run (NN): {nn_count_correct/(320-person_num)}")
-print(f"• Rata de Recunoastere Pentru Acest Run (kNN): {knn_count_correct/(320-person_num)}")
+print(f"• Timpul Aproximat De Executie Pentru Procesarea Unei Imagini {timpul_de_executie/person_num:.2f}s")
+print(f"• Matricea De Antrenare Contine : {400-person_num:.0f} imagini ")
+print(f"• Rata de Recunoastere Pentru Acest Run (NN): {nn_rate*100:.2f}")
+print(f"• Rata de Recunoastere Pentru Acest Run (kNN): {knn_rate*100:.2f}")
+
 
 #alg.displayImages(poza_test,knn_result,nn_result)
 
